da952ad327a9e2355ddb4779c593c1ec05666ebf
Cth1003 <ItsAMeCth@gmail.com>
2016-10-16 18:34:19 +0100

    aaa

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ var dete = new Date();
 var hours = dete.getHours();
 var mins = dete.getMinutes();
 //da discord mode : on
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
 
 window.onload = function () { 
 var links = document.getElementsByTagName("link"); for (var i = 0; i < links.length;i++) { var link = links[i]; if (link.rel === "stylesheet") {link.href += "?drink=bleach"; }}
@@ -28,6 +37,6 @@ setTimeout(function(){ sendnewmsg("J.J (furry and hairplane) bot", "reed the doc
 }
 function sendnewmsg(usrn, msg) {
 //o no jquery
-document.body.innerHtml += '<span class="username-wrapper"><strong class="user-name">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 -->' + msg + '<!-- /react-text --></div></div></div>';
-links = document.getElementsByTagName("link"); for (var i = 0; i < links.length;i++) { var link = links[i]; if (link.rel === "stylesheet") {link.href += "&drink=bleach"; }}
+var ono = create('<span class="username-wrapper"><strong class="user-name">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 -->' + msg + '<!-- /react-text --></div></div></div>');
+document.body.insertBefore(ono, document.body.childNodes[0]);
 }
