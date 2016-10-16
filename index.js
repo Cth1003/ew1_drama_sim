41999a0765f272c09730456dbec7ee330b5f3dba
Cth1003 <ItsAMeCth@gmail.com>
2016-10-16 17:47:21 +0100

    AAA

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ var dete = new Date();
 var hours = dete.getHours();
 var mins = dete.getMinutes();
 //da discord mode : on
-setTimeout(function(){ sendnewmsg("Epic // NO MSPAINT", "wirus"); }, 3000);
-setTimeout(function(){ sendnewmsg("FireC", "dextop"); }, 1000);
-setTimeout(function(){ sendnewmsg("Cth103", "taxbar"); }, 3000);
-setTimeout(function(){ sendnewmsg("pengu", "@Clyde audit logs pls"); }, 1000);
+setTimeout(function(){ sendnewmsg("Epic // NO MSPAINT", "wirus"); }, 10);
+setTimeout(function(){ sendnewmsg("FireC", "dextop"); }, 30);
+setTimeout(function(){ sendnewmsg("Cth103", "taxbar"); }, 50);
+setTimeout(function(){ sendnewmsg("pengu", "@Clyde audit logs pls"); }, 60);
 sendnewmsg("pengu", "a");
 sendnewmsg("pengu", "aa");
 sendnewmsg("pengu", "a");
-setTimeout(function(){ sendnewmsg("pengu", "aav"); }, 10);
+setTimeout(function(){ sendnewmsg("pengu", "aav"); }, 70);
 sendnewmsg("pengu", "aa");
-setTimeout(function(){ sendnewmsg("pengu", "av"); }, 10);
+setTimeout(function(){ sendnewmsg("pengu", "av"); }, 90);
 sendnewmsg("pengu", "a");
 sendnewmsg("pengu", "aaaa");
 sendnewmsg("pengu", "aa");
 sendnewmsg("pengu", "aaaa");
-setTimeout(function(){ sendnewmsg("pengu", "va"); }, 10);
+setTimeout(function(){ sendnewmsg("pengu", "va"); }, 100);
 sendnewmsg("pengu", "aa");
-setTimeout(function(){ sendnewmsg("pengu", "a"); }, 10);
+setTimeout(function(){ sendnewmsg("pengu", "a"); }, 120);
 sendnewmsg("J.J (furry and hairplane)", "jjbot/help");
-setTimeout(function(){ sendnewmsg("J.J (furry and hairplane) bot", "reed the docs to get help xdddddddddddddddddddd"); }, 8);
+setTimeout(function(){ sendnewmsg("J.J (furry and hairplane) bot", "reed the docs to get help xdddddddddddddddddddd"); }, 1000);
 function sendnewmsg(usrn, msg) {
 //o no jquery
 document.write('<span class="username-wrapper"><strong class="user-name">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 -->' + msg + '<!-- /react-text --></div></div></div>');
