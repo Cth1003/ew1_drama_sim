8ad17ee8e578bb1f8abe4b7b226d44ac9cb7ccdb
Cth1003 <ItsAMeCth@gmail.com>
2016-10-16 18:12:12 +0100

    dom

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var hours = dete.getHours();
 var mins = dete.getMinutes();
 //da discord mode : on
 
-document.addEventListener("DOMContentLoaded", function(event) {
+window.onload = function () { 
 setTimeout(function(){ sendnewmsg("Epic // NO MSPAINT", "wirus"); }, 1000);
 setTimeout(function(){ sendnewmsg("FireC", "dextop"); }, 2000);
 setTimeout(function(){ sendnewmsg("Cth103", "taxbar"); }, 3000);
@@ -24,7 +24,7 @@ setTimeout(function(){ sendnewmsg("pengu", "aa"); }, 5020);
 setTimeout(function(){ sendnewmsg("pengu", "a"); }, 5030);
 setTimeout(function(){ sendnewmsg("J.J (furry and hairplane)", "jjbot/help"); }, 10000);
 setTimeout(function(){ sendnewmsg("J.J (furry and hairplane) bot", "reed the docs to get help xdddddddddddddddddddd"); }, 20000);
-  });
+}
 function sendnewmsg(usrn, msg) {
 //o no jquery
 document.write('<span class="username-wrapper"><strong class="user-name">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 -->' + msg + '<!-- /react-text --></div></div></div>');
