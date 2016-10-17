64c42ef5eb8920ec8c01310b09e10400443537d2
Cth1003 <ItsAMeCth@gmail.com>
2016-10-17 09:28:47 +0100

    firc

--- index.js
+++ index.js
@@ -32,11 +32,10 @@ setTimeout(function(){ sendnewmsg("pengu", "aaaa"); }, 5000);
 setTimeout(function(){ sendnewmsg("pengu", "va"); }, 5001);
 setTimeout(function(){ sendnewmsg("pengu", "aa"); }, 5020);
 setTimeout(function(){ sendnewmsg("pengu", "a"); }, 5030);
-setTimeout(function(){ sendnewmsg("J.J (furry and hairplane)", "jjbot/help"); }, 10000);
-setTimeout(function(){ sendnewmsg("J.J (furry and hairplane) bot", "reed the docs to get help xdddddddddddddddddddd"); }, 20000);
+setTimeout(function(){ sendnewmsg("J.J", "jjbot/help"); }, 10000);
+setTimeout(function(){ sendnewmsg("J.J bot", "reed the docs to get help xdddddddddddddddddddd"); }, 20000);
 }
 function sendnewmsg(usrn, msg) {
-//o no jquery
-var ono = create('<span class="username-wrapper"><strong class="user-name">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 -->' + msg + '<!-- /react-text --></div></div></div>');
+if usrn.startsWith("FireC") var ono = create('<img src="firc.PNG"><strong class="user-name"><span class="username-wrapper"><strong class="user-name">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 -->' + msg + '<!-- /react-text --></div></div></div>');
 document.body.appendChild(ono);
 }
