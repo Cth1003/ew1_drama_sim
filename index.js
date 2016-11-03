8a3781a7edac87c9e7fdd7342dc635fa4d67dbd1
Cth103 <ItsAMeCth@gmail.com>
2016-11-03 13:51:09 +0000

    meisbored

--- index.js
+++ index.js
@@ -23,18 +23,24 @@ setTimeout(function(){ sendnewmsg("pengu", "a"); }, 5000);
 setTimeout(function(){ sendnewmsg("pengu", "aa"); }, 5000);
 setTimeout(function(){ sendnewmsg("J.J", "jjbot/help"); }, 6000);
 setTimeout(function(){ sendnewmsg("J.J bot", "reed the docs to get help xdddddddddddddddddddd"); }, 6500);
-setTimeout(function(){ sendnewmsg("pengu", "JULLIA I WILL KMS IF YOU DONT GIVE ME OWNER"); }, 6900);
-setTimeout(function(){ sendnewmsg("pengu", "JULLIA I WILL KMS IF YOU DONT GIVE ME OWNER"); }, 7000);
-setTimeout(function(){ sendnewmsg("pengu", "JULLIA I WILL KMS IF YOU DONT GIVE ME OWNER"); }, 7500);
-setTimeout(function(){ sendnewmsg("System Restore", "no pengu i wont give you owner"); }, 8000);
+setTimeout(function(){ sendnewmsg("pengu", "JULIA I WILL KMS IF YOU DONT GIVE ME OWNER"); }, 6900);
+setTimeout(function(){ sendnewmsg("pengu", "JULIA I WILL KMS IF YOU DONT GIVE ME OWNER"); }, 7000);
+setTimeout(function(){ sendnewmsg("pengu", "JULIA I WILL KMS IF YOU DONT GIVE ME OWNER"); }, 7500);
+setTimeout(function(){ sendnewmsg("System Restore", "NO pengu i wont give you owner"); }, 8000);
 }
 function sendnewmsg(usrn, msg) {
 if (usrn.startsWith("FireC")){
     var ono = create('<strong class="user-name"><span class="username-wrapper"><strong class="user-name"><img src="firec.PNG">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 --> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp &nbsp;' + msg + '<!-- /react-text --></div></div></div>');
 } else if (usrn.startsWith("Epic //") || usrn.startsWith("Cth103")) {
      var ono = create('<strong class="user-name"><span class="username-wrapper"><strong class="user-name"><img src="red.PNG">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 --> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;' + msg + '<!-- /react-text --></div></div></div>');
+} else if (usrn.startsWith("System Restore") || usrn.startsWith("electronicwiz1")) {
+     var ono = create('<strong class="user-name"><span class="username-wrapper"><strong class="user-name"><img src="jollya.png">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 --> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;' + msg + '<!-- /react-text --></div></div></div>');
+} else if (usrn.startsWith("J.J") || usrn.startsWith("JJ")) {
+     var ono = create('<strong class="user-name"><span class="username-wrapper"><strong class="user-name"><img src="jj.png">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 --> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;' + msg + '<!-- /react-text --></div></div></div>');
+} else if (usrn.startsWith("pengu") || usrn.startsWith("pingu")) {
+     var ono = create('<strong class="user-name"><span class="username-wrapper"><strong class="user-name"><img src="pengu.png">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 --> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;' + msg + '<!-- /react-text --></div></div></div>');
 } else {
-    var ono = create('<strong class="user-name"><span class="username-wrapper"><strong class="user-name">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 --> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;' + msg + '<!-- /react-text --></div></div></div>');
+    var ono = create('<strong class="user-name"><span class="username-wrapper"><strong class="user-name"><img src="red.PNG">' + usrn + '</strong></span><span class="highlight-separator"> - </span><span class="timestamp">Today at ' + hours + ":" + mins +'</span></h2><div class="message-text"><div class="btn-option"></div><div class="markup"><!-- react-text: 12191 --> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;' + msg + '<!-- /react-text --></div></div></div>');
 }
 document.body.appendChild(ono);
 }
